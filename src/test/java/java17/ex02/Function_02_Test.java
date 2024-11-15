837d872dfd519588d231c69b7096c59053e99708
Cooljeb <joseph.brou@gmail.com>
2024-11-15 01:07:58 +0100

    TP Function _01_02_03

--- src/test/java/java17/ex02/Function_02_Test.java
+++ src/test/java/java17/ex02/Function_02_Test.java
@@ -15,20 +15,28 @@ public class Function_02_Test {
     //  tag::buildAccount[]
     // TODO Compléter la fonction buildAccount
     // TODO la fonction possède 2 paramètres en entrée : une personne et un solde
-    BiFunction<Person, Integer, Account> buildAccount = null;
+    BiFunction<Person, Integer, Account> buildAccount (Person p, Integer a) {
+        return (pers, s) -> {
+            Account account = new Account();
+            account.setBalance(a);
+            account.setOwner(p);
+            return account;
+        };
+
+    }
     //  end::buildAccount[]
 
     @Test
     public void test_build_account() throws Exception {
 
         // TODO invoquer la fonction buildAccount pour que le test soit passant
-        Account account = null;
+        Person person = new Person("John", "France", 80, "pass");
+        Account result = buildAccount(person, 500).apply(person,500);;
 
-        assert account.getBalance().equals(500);
-        assert account.getOwner().getFirstname().equals("John");
-        assert account.getOwner().getLastname().equals("France");
-        assert account.getOwner().getAge().equals(80);
-        assert account.getOwner().getPassword().equals("pass");
+        assert result.getOwner().getFirstname().equals("John");
+        assert result.getOwner().getLastname().equals("France");
+        assert result.getOwner().getAge().equals(80);
+        assert result.getOwner().getPassword().equals("pass");
     }
 
 
